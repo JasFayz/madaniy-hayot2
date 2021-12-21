fa5af1a965a7eb3b4e6ad49d42c988d305761c75
JasFayz <jasfayz@gmail.com>
2021-12-21 14:19:20 +0500

    Add changes

--- js/common.js
+++ js/common.js
@@ -5,29 +5,44 @@ document.addEventListener('DOMContentLoaded', function () {
 		$('.top_nav').toggleClass('show');
 	})
 
-	$('#afisha__slider').owlCarousel({
-		loop: true,
-		items: 4,
-		dots: false,
-		autoplay: true,
-		slideTransition: 'linear',
-		autoplayTimeout: 3000,
-		autoplaySpeed: 3000,
-		autoplayHoverPause: true,
-		nav: false,
-		responsive: {
-			0: {
-				items: 1
-			},
-			700: {
-				items: 2
-			},
-			900: {
-				items: 3
-			},
-			1400: {
-				items: 4
-			}
+	// $('#afisha__slider').owlCarousel({
+	// 	loop: true,
+	// 	items: 4,
+	// 	dots: false,
+	// 	autoplay: true,
+	// 	slideTransition: 'linear',
+	// 	autoplayTimeout: 3000,
+	// 	autoplaySpeed: 3000,
+	// 	autoplayHoverPause: true,
+	// 	nav: false,
+	// 	responsive: {
+	// 		0: {
+	// 			items: 1
+	// 		},
+	// 		700: {
+	// 			items: 2
+	// 		},
+	// 		900: {
+	// 			items: 3
+	// 		},
+	// 		1400: {
+	// 			items: 4
+	// 		}
+	// 	}
+	// })
+
+
+	function main_nav_mobile() {
+		if ($(document).width() <= 768) {
+			$('.main__nav .nav li:not(:first-child):not(.mobile-collapse)').each(function (index, el) {
+				$(el).hide()
+			})
 		}
-	})
+		$('.mobile-collapse').click(function () {
+			$('.main__nav .nav li:not(:first-child):not(.mobile-collapse)').each(function (index, el) {
+				$(el).toggle()
+			})
+		})
+	}
+	main_nav_mobile()
 })
